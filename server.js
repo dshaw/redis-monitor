94622ae66f1334165237e725d9452c12bc049dae
Daniel D. Shaw <github@dshaw.com>
2012-06-29 23:42:22 -0700

    Update server with new `redis-monitor` plumbing.

--- server.js
+++ server.js
@@ -12,16 +12,18 @@ var path = require('path')
   , optimist = require('optimist')
   , sio = require('socket.io')
   , tako = require('tako')
-  , redisMonitor = require('./redis-monitor')
+  , RedisMonitor = require('./redis-monitor')
 
 /**
  * Configuration.
  */
 
 var options = optimist.default({ httpport: 8888, announce: true }).argv
-  , client = redisMonitor(options)
+  , redisMonitor = RedisMonitor(options)
+  , client = redisMonitor.announceClient
   , redisStore = new sio.RedisStore({ nodeId: function () { return options.nodeid } })
   , app = tako({ socketio: sio })
+  , debug = (options.debug) ? console.log : function noop () {}
 
 /**
  * Socket.io.
@@ -33,7 +35,7 @@ app.socketioManager.configure(function () {
 
 app.sockets.on('connection', function (socket) {
   // join the "rti" room to get announce broadcasts.
-  socket.join(client.rti.room)
+  socket.join(redisMonitor.rti.room)
 
   rtiList(socket, client, function () {})
 
@@ -41,11 +43,13 @@ app.sockets.on('connection', function (socket) {
     rtiList(socket, client)
   })
 
-  socket.on('rti:info', function sendInfo (name) {
-    console.log('rti:info received', arguments)
+  socket.on('rti:info', function (name) {
+    debug('rti:info received', name)
+
     client.get(name, function (err, info) {
       if (err) return console.error(err)
-      console.log('get info', err, info)
+
+      debug('get info', err, info)
       socket.emit(name, JSON.parse(info))
     })
   })
@@ -59,7 +63,8 @@ app.route('/').files(path.join(__dirname, 'public'))  // WTF, Tako?
 app.route('/*').files(path.join(__dirname, 'public'))
 
 app.httpServer.listen(options.httpport, function onListening () {
-  console.log('listening on :', app.httpServer.address())
+  var address = app.httpServer.address()
+  console.log('Server listening on %s:%d', (address.address == '0.0.0.0') ? 'localhost' : address.address, address.port)
 })
 
 
